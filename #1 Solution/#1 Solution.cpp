122d8e11c4f46cb5dc5404c162695aaa5075c2bc
AhmeddAyman <ahmedaiman12312312@gmail.com>
2024-11-28 21:30:43 +0200

    Add project files.

---
+++ #1 Solution/#1 Solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+// #Course 4 Solution 1
+// Print Name.
+
+void PrintName(string Name) 
+{
+	cout << "\n Your Name is : " << Name << endl;
+}
+
+int main()
+{
+	PrintName("Ahmed");
+}
+
